0468d6d20fe31817d7650d1919a4a04b7b4b8c12
JESSTADA <jesstada@gmail.com>
2019-01-23 22:54:37 +1100

    displays bounty inputs on webpage

--- client/src/components/DisplayBounties.js
+++ client/src/components/DisplayBounties.js
@@ -7,21 +7,36 @@ export default class DisplayBounties extends React.Component {
     this.state = {
       title: "Not real title",
       description: "",
-      price: 12,
-      // ownerAddress: "",
+      price: 2,
       accepted: false
     }
   }
 
   async componentDidMount() {
     let bounty = await this.props.instance.methods.getBountyById(this.props.id).call({from: this.props.ownerAddress});
-    console.log('bounty', bounty);
+    console.log('bounty.title', bounty.title);
+    console.log('bounty.description', bounty.description);
+    console.log('bounty.price', bounty.price);
+    console.log('bounty.accepted', bounty.accepted);
+
+    this.setState({
+      title: bounty.title,
+      description: bounty.description,
+      price: bounty.price,
+      accepted: bounty.accepted
+    })
+
+    console.log('display this.state', this.state)
   }
 
   render(props) {
     return(
       <div>
       <h3>Display Created Bounties</h3>
+      <p>{this.state.title}</p>
+      <p>{this.state.description}</p>
+      <p>{this.state.price}</p>
+      <p>{this.state.accepted}</p>
       </div>
     )
   }
